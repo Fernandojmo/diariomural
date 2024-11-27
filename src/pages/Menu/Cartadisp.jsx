1d1b71e1ab1e500f99ffe70db8c18f86a3e440a7
Fernandojmo <fernando.javier.melo@gmail.com>
2024-11-27 11:05:25 -0300

    se agrega calendario y modal

--- src/pages/Menu/Cartadisp.jsx
+++ src/pages/Menu/Cartadisp.jsx
@@ -40,18 +40,12 @@ const Cartadisp = () => {
             >
                 <Modal.Header closeButton>
                     <Modal.Title id="example-custom-modal-styling-title">
-                        Custom Modal Styling
+                        Estamos comenzando y abiertos a sugerencias
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                 <p>
-                    Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
-                    commodi aspernatur enim, consectetur. Cumque deleniti temporibus
-                    ipsam atque a dolores quisquam quisquam adipisci possimus
-                    laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
-                    accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
-                    reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
-                    deleniti rem!
+                    Escríbenos a diariomuralcurico@gmail.com
                 </p>
                 </Modal.Body>
             </Modal>
